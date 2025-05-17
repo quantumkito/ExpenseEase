f36744a8129f007bce98a444776837f0003f1fbb
quantumkito <mayakumari20082016@gmail.com>
2025-05-17 14:46:22 +0530

    Updating code

--- ExpenseEase.java
+++ ExpenseEase.java
@@ -11,7 +11,31 @@ class Expense {
         if (amount <= 0) {
             throw new  IllegalArgumentException("Amount must be positive!");
         }
+
+        this.category = category;
+        this.description = description;
+        this.amount = amount;
+        this.date = LocalDate.now();
     }
+
+    public String getCategory() {
+        return category;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + date + "] " + category + " - " + description + ": ₹" + amount;
+    }
+
 }
 
 
