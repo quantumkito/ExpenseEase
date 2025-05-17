ea1f001caa655a958a3043a6b1b0d19144c3ffc0
quantumkito <mayakumari20082016@gmail.com>
2025-05-17 14:35:14 +0530

    Updating code

--- ExpenseEase.java
+++ ExpenseEase.java
@@ -1,1 +1,18 @@
 import java.util.ArrayList;
+import java.time.LocalDate;
+
+class Expense {
+    private String category;
+    private String description;
+    private double amount;
+    private LocalDate date;
+
+    public Expense (String category, String description, double amount) {
+        if (amount <= 0) {
+            throw new  IllegalArgumentException("Amount must be positive!");
+        }
+    }
+}
+
+
+
